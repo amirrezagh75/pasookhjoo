d1a76882693171e7b78ff47bab79d13566965ef3
Killian1996 <amirghorbani1996@gmail.com>
2021-02-23 17:41:10 +0330

    f

---
+++ components/Nav.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="navbar">
+        <nav>
+            <router-link href="">HTML</router-link> 
+            <router-link href="">CSS</router-link> 
+            <router-link href="">JavaScript</router-link> 
+            <router-link href="">Python</router-link>
+        </nav>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
